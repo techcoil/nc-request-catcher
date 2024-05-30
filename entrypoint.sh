0423756639e5db1fa96a6b71523e351e9b5beda1
Yaron Uliel <5229872+yaronuliel@users.noreply.github.com>
2024-05-30 18:17:34 +0300

    Fix nc script to support alpine version of nc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env sh 
 
-PORT="${PORT:-3000}"
+PORT="${PORT:-3000}" 
 
 echo "Starting request catcher on port ${PORT}"
 
 while true; do 
-   echo "HTTP/1.1 200 OK\nContent-Length: 15\nContent-Type: text/plain;charset=utf-8\n\nrequest caught" | nc -l $PORT
+   echo "HTTP/1.1 200 OK\nContent-Length: 15\nContent-Type: text/plain;charset=utf-8\n\nrequest caught" | nc -l -p $PORT -s 0.0.0.0
    [ "$?" != "0" ] && break
-   echo "\n\n\n\n====\n\n\n\n"
+   printf "\n\n====Completed at $(date)====\n\n"
 done
