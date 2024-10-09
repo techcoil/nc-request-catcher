588d3ed4c844c043b526c239447627ac7629617e
Yaron Uliel <5229872+yaronuliel@users.noreply.github.com>
2024-10-09 14:35:27 +0300

    Fix catcher response

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ PORT="${PORT:-3000}"
 echo "Starting request catcher on port ${PORT}"
 
 while true; do 
-   echo "HTTP/1.1 200 OK\nContent-Length: 15\nContent-Type: text/plain;charset=utf-8\n\nrequest caught" | nc -l -p $PORT -s 0.0.0.0
+   printf "HTTP/1.1 200 OK\nContent-Length: 15\nContent-Type: text/plain;charset=utf-8\n\nrequest caught\n" | nc -l -p $PORT -s 0.0.0.0
    [ "$?" != "0" ] && break
    printf "\n\n====Completed at $(date)====\n\n"
 done
